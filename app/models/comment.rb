dd55ec2089d25c7c97aa3ed9a0b665cc2a943daa
Valentina Maldonado <c.valentinamm@gmail.com>
2016-06-16 18:39:16 -0400

    Se eliminó la validación para hacer la propiedad anonymous obligatoria

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,5 @@ class Comment < ActiveRecord::Base
   belongs_to :author, class_name: "User"
   belongs_to :post
   validates :content, presence: true, length: { maximum: 500 }
-  validates :anonymous, presence: true, length: { maximum: 20 }
+  validates :anonymous, length: { maximum: 20 }
 end
